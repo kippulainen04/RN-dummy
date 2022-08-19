955044712ce1d9b4e40ccd390520af692a3f0f36
Nhan Phan <kippulainenko@gmail.com>
2022-08-19 11:43:01 +0300

    first commit

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View>      
+        <Text style={styles.dummyText}>Hello Benaraaad!</Text>
+      </View>
+      <Text style={styles.dummyText}>How about your new job!</Text>
       <StatusBar style="auto" />
+      <Button title='Tap me!'/>
     </View>
   );
 }
@@ -17,4 +21,10 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  dummyText: {
+    margin: 16, 
+    borderWidth: 2, 
+    borderColor: 'red', 
+    padding: 16
+  }
 });
---
+++ README.md
@@ -0,0 +1,2 @@
+# RN-dummy
+This is just a dummy React native project to learn basic React Native functionalities.
